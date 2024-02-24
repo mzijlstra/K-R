41e6b61fc21a945d850c7b5695959076b9ef1c19
Michael Zijlstra <mzijlstra@gmail.com>
2024-02-24 16:35:14 +0100

    Fix function name

--- ch4/12.c
+++ ch4/12.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-/* printd: print n in decimal */
-int printd(int n, char s[], int i) {
+/* atoi: print n in decimal */
+int atoi(int n, char s[], int i) {
     if (n < 0) {
         s[i++] = '-';
         n = -n;
     }
     if (n / 10) {
-        i = printd(n / 10, s, i);
+        i = atoi(n / 10, s, i);
     }
 
     s[i] = n % 10 + '0';
@@ -19,6 +19,6 @@ int printd(int n, char s[], int i) {
 
 int main() {
     char s[100];
-    printd(-123, s, 0);
+    atoi(-123, s, 0);
     printf("%s\n", s);
 }
